0af7e881b0729a9fa28ce0a1c2690af398eecc0c
Misael Corvera <mcmalfaro@hotmail.com>
2019-06-03 16:11:54 -0700

    using embeded

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
-        <div class="content">
-            resume
+        <div class="content test">
+           <embed src="../assets/myResume.pdf" type="application/pdf" width="100%" height="600px" />
         </div>
         
     </div>
 </template>
+
+<script>
+   
+</script>
+
+<style scoped>
+
+</style>
